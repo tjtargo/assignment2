75d2735943042d2ab92cf0b0b0ec1c9f9ff2c499
jphilly <jdphillips93@gmail.com>
2016-02-12 17:37:43 -0600

    Still broken, am clueless

--- customer_add.php
+++ customer_add.php
@@ -1,41 +1,37 @@
 <?php //customer_add.php
-
-$hn = 'www.it354.com';
-$db = 'it354_students';
-$un = 'it354_students';
-$pw = 'steinway';
-
-$conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die($conn->connect_error);
-
- if ($_POST['submitbutton']){
-	
-		$fname 		= get_post($conn, 'fname');
-		$lname 		= get_post($conn, 'lname');
-		$address 	= get_post($conn, 'address');
-		$city 		= get_post($conn, 'city');
-		$state 		= get_post($conn, 'state');
-		$zip 		= get_post($conn, 'zip');
-		$email 		= get_post($conn, 'email');
-		$phone 		= get_post($conn, 'phone');
 		
-		$query	= "INSERT INTO customers VALUES" . 
-		"('$fname', '$lname', '$address', '$city', '$state', '$zip', '$email', '$phone')";
+		$hn = 'www.it354.com';
+		$db = 'it354_students';
+		$un = 'it354_students';
+		$pw = 'steinway';
 		
-		$result = $conn->query($query);
-		if(!$result) die ("Database access failed: " . $conn->error);
-
- }
-$result->close();
-$conn->close();
+		$conn = mysqli_connect($hn, $db, $un, $pw);
+		
+		if (!$conn) {
+			echo "Error: Unable to connect to MySQL." . PHP_EOL;
+			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+			exit;
+		}
+		
+		echo $_POST['fname'];
+		
+		if (isset($_POST['submitbutton'])){
+			
+			$fname = $_POST['fname'];
+			$lname = $_POST['lname'];
+			$address = $_POST['address'];
+			$city = $_POST['city'];
+			$state = $_POST['state'];
+			$zip = $_POST['zip'];
+			$email = $_POST['email'];
+			$phone = $_POST['phone'];
+			
+			$query	= "INSERT INTO customers VALUES ('$fname', '$lname', '$address', '$city', '$state', '$zip', '$email', '$phone')";
+			
+			$sql = mysqli_query($conn, $query);
+			
+		}
+		mysqli_close($conn);
 ?>
-
-
-<!-- if (isset($_POST['fname']) 		&&
-	isset($_POST['lname']) 		&&
-	isset($_POST['address']) 	&&
-	isset($_POST['city']) 		&&
-	isset($_POST['state']) 		&&
-	isset($_POST['zip']) 		&&
-	isset($_POST['email']) 		&&
-	isset($_POST['phone']) -->
+	
