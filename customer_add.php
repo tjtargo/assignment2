e82526258fd798d72403e73329950b5ef8255142
tomtargosz <tom.targosz@yahoo.com>
2016-02-12 18:43:00 -0600

    Added Upload Capabilities
    Page now adds record to the database

--- customer_add.php
+++ customer_add.php
@@ -8,34 +8,20 @@ $pw = 'steinway';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
 
- if ($_POST['submitbutton']){
-	
-		$fname 		= get_post($conn, 'fname');
-		$lname 		= get_post($conn, 'lname');
-		$address 	= get_post($conn, 'address');
-		$city 		= get_post($conn, 'city');
-		$state 		= get_post($conn, 'state');
-		$zip 		= get_post($conn, 'zip');
-		$email 		= get_post($conn, 'email');
-		$phone 		= get_post($conn, 'phone');
-		
-		$query	= "INSERT INTO customers VALUES" . 
-		"('$fname', '$lname', '$address', '$city', '$state', '$zip', '$email', '$phone')";
-		
-		$result = $conn->query($query);
-		if(!$result) die ("Database access failed: " . $conn->error);
+$stmt = $conn->prepare("INSERT INTO customers (firstname, lastname, address, city, state, zip, email, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
- }
-$result->close();
+$fname = $lname = $address = $city = $state = $zip = $email = $phone = '';
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$stmt->bind_param("ssssssss", $fname, $lname, $address, $city, $state, $zip, $email, $phone);
+
+$stmt->execute();
+$stmt->close();
 $conn->close();
 ?>
-
-
-<!-- if (isset($_POST['fname']) 		&&
-	isset($_POST['lname']) 		&&
-	isset($_POST['address']) 	&&
-	isset($_POST['city']) 		&&
-	isset($_POST['state']) 		&&
-	isset($_POST['zip']) 		&&
-	isset($_POST['email']) 		&&
-	isset($_POST['phone']) -->
